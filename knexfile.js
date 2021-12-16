e270bad6bfaa5498b6e48934cb6c208e42f86d09
WaylayOvercast <timelyovercast@gmail.com>
2021-12-16 13:51:05 -0600

    changed some settings, user auth working fine, starting testing

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,18 @@
-// Update with your config settings.
+
+
+const sharedConfig = {
+  client: 'sqlite3',
+  useNullAsDefault: true,
+  migrations: { directory: './data/migrations' },
+  pool: { afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done) },
+}
 
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
+    ...sharedConfig,
+    connection: { filename: './data/database.db3' },
+    seeds: { directory: './data/seeds' },
   },
 
   staging: {
