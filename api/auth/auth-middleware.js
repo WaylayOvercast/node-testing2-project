5673d55b7a05f3f6447a6e9026898868ae283699
WaylayOvercast <timelyovercast@gmail.com>
2021-12-16 19:42:19 -0600

    majority of tests passing, middleware rebuilt

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,5 +1,5 @@
 const Users = require('../users/users-model')
-
+const bcrypt = require('bcryptjs')
 
 function restricted(req, res, next) {
   
@@ -7,53 +7,65 @@ function restricted(req, res, next) {
       
       next()
     } else {
-      next(res.status(401).json({message: "You shall not pass!"}))
+      res.status(401).json({message: "You shall not pass!"})
     }
 }
 
 
-async function checkUsernameFree(req, res, next) {
-    try{
-        const users = await Users.findBy({username: req.body.username})
-        if (!users.length){
-        next()
-        }else {
-        next({ status: 422, message: 'Username taken'})
-        }
-    }catch (err){
-        next(err)
-  }
-}
-
-
-async function checkUsernameExists(req ,res, next) {
+async function checkSubmission(req ,res, next) {
     try{ 
-      const users = await Users.findBy({username: req.body.username})
-    if (users.length){
-      next()
-    }else{
-      next({ status: 401, message: 'Invalid credentials'})
-    }
+        let users = []
+        let pass 
+      if(req.body.password){
+         pass = req.body.password
+        if (pass.length <= 3){
+          next({status:422, message:"Password must be longer than 3 chars"})
+    
+        }else if (!req.body.username){
+          next({status:422, message:'You must have a username'})
+        }else{
+          users = await Users.findBy({username: req.body.username})
+          if (!users.length){
+            next()
+          }else{
+            next({status:403, message: 'That name is already taken'})
+          }
+        }
+      }else{
+        next({status:422, message:'You must have a password'})
+      } 
+      
    }catch (err) {
     next(err)
  }
 }
 
+async function checkLogin (req ,res, next){
+  try{
+      
+      if(!req.body.username || !req.body.password){
+        next({status:400, message:'invalid request - make sure all fields are there'})
 
-function checkPasswordLength(req, res, next) {
-
-  const pass = req.body.password
-  if (!pass || pass.length <= 3){
-    next(res.status(422).json({ message : "Password must be longer than 3 chars"}))
-  }else {
-    next()
+      }else{
+        const [userFromDb] = await Users.findBy({username: req.body.username})
+        if(!userFromDb){
+          next({status:404, message:'user not found'})
+  
+        }else if(bcrypt.compareSync(req.body.password, userFromDb.password)){
+          req.session.user = userFromDb
+          next()
+        }else{
+          next({status:400, message:'Invalid credentials'})
+        }
+      }
+  }catch(err){
+    next(err)
   }
 }
 
 
 module.exports = {
-    checkUsernameFree,
     restricted,
-    checkUsernameExists,
-    checkPasswordLength
+    checkLogin,
+    checkSubmission
   }
